f323958a484343b903dc30bd014c73071231b83f
Abedallah-Sako <127647063+Abedallah-Sako@users.noreply.github.com>
2025-03-19 15:25:55 +0300

    - fix exception

--- app/src/main/java/com/imagine/android_imagine_tools/MainActivity.kt
+++ app/src/main/java/com/imagine/android_imagine_tools/MainActivity.kt
@@ -1,27 +1,10 @@
 package com.imagine.android_imagine_tools
 
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.Typeface
-import android.icu.lang.UProperty.INT_START
 import android.os.Bundle
-import android.provider.Settings
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.StyleSpan
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat
-import androidx.core.text.toSpannable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.imagine.android_imagine_tools.tools.ext.logd
-import com.imagine.android_imagine_tools.tools.ext.toPx
-import com.imagine.android_imagine_tools.tools.ui.fragments.webView.NotificationRequestFragment
-import com.imagine.android_imagine_tools.tools.ui.views.customAlertDialog.CustomAlertDialogBuilder
-import com.imagine.android_imagine_tools.tools.ui.views.imageTextList.ImageTextList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
