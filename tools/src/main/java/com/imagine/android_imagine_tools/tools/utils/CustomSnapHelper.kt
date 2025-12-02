0efcf7cd58762feb850068f94af1b6ad4351bdeb
Abedallah-Sako <127647063+Abedallah-Sako@users.noreply.github.com>
2025-12-02 15:55:06 +0300

    handle tablets in custom snap helper (not perfect but works)

--- tools/src/main/java/com/imagine/android_imagine_tools/tools/utils/CustomSnapHelper.kt
+++ tools/src/main/java/com/imagine/android_imagine_tools/tools/utils/CustomSnapHelper.kt
@@ -1,11 +1,39 @@
 package com.imagine.android_imagine_tools.tools.utils
 
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 
 class CustomSnapHelper : PagerSnapHelper() {
     //snaps to the start of the view
+    private var attachedRecyclerView: RecyclerView?= null
+
+    override fun attachToRecyclerView(recyclerView: RecyclerView?) {
+        super.attachToRecyclerView(recyclerView)
+        attachedRecyclerView = recyclerView
+    }
+
+    override fun findSnapView(layoutManager: RecyclerView.LayoutManager?): View? {
+        val smallestDp = attachedRecyclerView?.resources?.configuration?.smallestScreenWidthDp?:0
+
+        //tablets/foldable
+        if(smallestDp >= 600){
+            val lm = layoutManager as? LinearLayoutManager
+            val firstCompletelyVisibleViewPosition = lm?.findFirstCompletelyVisibleItemPosition()
+
+            if(firstCompletelyVisibleViewPosition == -1 || firstCompletelyVisibleViewPosition == null){
+                return super.findSnapView(layoutManager)
+            }else{
+                return lm.findViewByPosition(firstCompletelyVisibleViewPosition)
+            }
+        }else{
+            return super.findSnapView(layoutManager)
+        }
+
+
+
+    }
 
     override fun calculateDistanceToFinalSnap(
         layoutManager: RecyclerView.LayoutManager,
@@ -17,8 +45,7 @@ class CustomSnapHelper : PagerSnapHelper() {
         out[1] = layoutManager.getDecoratedTop(targetView) - layoutManager.paddingTop
 
         if (layoutManager.layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-            out[0] =
-                (layoutManager.getDecoratedRight(targetView) - layoutManager.paddingRight) - layoutManager.width
+            out[0] = (layoutManager.getDecoratedRight(targetView) - layoutManager.paddingRight) - layoutManager.width
             out[1] = layoutManager.getDecoratedTop(targetView) - layoutManager.paddingTop
         }
 
